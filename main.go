db7a006e83e6261d011fdbfc7ffc5f9e9daf9131
pro <rafts_mopiest.0y@icloud.com>
2023-12-19 17:46:21 -0500

    working on readGoogleTrends function

--- main.go
+++ main.go
@@ -1,33 +1,33 @@
 package main
 
 import (
-	"fmt"
 	"encoding/xml"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
 )
 
 type RSS struct {
-	XMLName		xml.Name	`xml:"rss"`
-	Channel		*Channel	`xml:"channel"`
+	XMLName xml.Name `xml:"rss"`
+	Channel *Channel `xml:"channel"`
 }
 
 type Channel struct {
-	Title		string		`xml:"title"`
-	ItemList	[]Item		`xml:"item"`
+	Title    string `xml:"title"`
+	ItemList []Item `xml:"item"`
 }
 
 type Item struct {
-	Title		string		`xml:"title"`
-	Link		string		`xml:"link"`
-	Traffic		string		`xml:"approx_traffic"`
-	NewsItems	[]News		`xml:"news_item"`
+	Title     string `xml:"title"`
+	Link      string `xml:"link"`
+	Traffic   string `xml:"approx_traffic"`
+	NewsItems []News `xml:"news_item"`
 }
 
 type News struct {
-	Headline		string	`xml:"news_item_title"`
-	HeadlineLink	string	`xml:"news_item_url"`
+	Headline     string `xml:"news_item_title"`
+	HeadlineLink string `xml:"news_item_url"`
 }
 
 func main() {
@@ -38,14 +38,22 @@ func main() {
 func getGoogleTrends() *http.Response {
 	resp, err := http.Get("https://trends.google.com/trends/trendingsearches/daily/rss?geo=US")
 
-	if err != nil{
+	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+
+	return resp
 }
 
 func readGoogleTrends() []byte {
-	getGoogleTrends
+	resp := getGoogleTrends()
+	data, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	return data
 }
-
-
