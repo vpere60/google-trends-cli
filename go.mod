73e66f44ebb698474d193aaffaf7a45296ab9ef1
pro <rafts_mopiest.0y@icloud.com>
2023-12-19 16:19:42 -0500

    project outline

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vpere60/google-trends-cli.git
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"encoding/xml"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	fmt.Println("Hello world!")
+}
+
+func main() {
+	readGoogleTrends
+}
+
+func readGoogleTrends() {
+	getGoogleTrends
+}
+
+func getGoogleTrends() {
+
+}
